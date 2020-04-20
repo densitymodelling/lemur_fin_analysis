2051ffdbf66a2c4398bf40c31d50210cb70d6d43
dave <dave@bluewhale.st-andrews.ac.uk>
2020-04-20 12:49:14 +0100

    test

--- 0_format_aux_data.R
+++ 0_format_aux_data.R
@@ -8,7 +8,5 @@ cce_poly$x <- cce_poly$x-360
 # load the prediction grid areas
 pred_areas <- read.csv("data/Grid_Lat30to48_Lon226to244.5_Step0.1withArea.csv")
 
-
-
-
+# save data
 save(cce_poly, pred_areas, file="RData/0_format_aux_data.RData")

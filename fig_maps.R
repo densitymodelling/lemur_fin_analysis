33a9f83771c1039ca9ad738e8ad87e1fa1a4dc3e
dave <dave@bluewhale.st-andrews.ac.uk>
2020-07-08 17:46:37 +0100

    more plot fiddling

--- fig_maps.R
+++ fig_maps.R
@@ -23,15 +23,18 @@ p_pred <- ggplot(summary_predgrid_all, aes(y=mlat, x=mlon)) +
   geom_polygon(data = w, aes(x = long, y = lat, group = group), fill = "grey80") +
   #geom_tile(aes(fill=avv_d)) +
   geom_contour_filled(aes(z=avv), breaks=colour_cats,
-                      limits=c(0,1), show.legend = TRUE) +
+                      show.legend = TRUE) +
   geom_point(aes(y=mlat, x=mlon), data=subset(fin, count>0),
              shape = 21, colour = "black", fill = "white", size=0.75) +
-  scale_fill_viridis_d() +
+  scale_fill_viridis_d(drop=FALSE) +
   labs(x="", y="", fill="Density") +
   coord_map(ylim=range(summary_predgrid_all$mlat),
             xlim=range(summary_predgrid_all$mlon)) +
   theme_minimal() +
-  theme(legend.position = "bottom")
+  theme(legend.position = "bottom",
+        legend.text = element_text(size=12),
+        legend.title = element_text(size=12),
+        axis.text = element_text(size=12))
 print(p_pred)
 
 ggsave(p_pred, file="figures/pred_d.pdf", width=7, height=9)
@@ -54,7 +57,7 @@ ggsave(p_pred, file="figures/pred_d.pdf", width=7, height=9)
 #ggsave(p_sd, file="figures/unc_d.pdf", width=7, height=9)
 
 # standard deviations with g0
-summary_predgrid_all$sdd_g0_d <- cut(summary_predgrid_all$sdd_g0, colour_cats)
+#summary_predgrid_all$sdd_g0_d <- cut(summary_predgrid_all$sdd_g0, colour_cats)
 
 p_sd_g0 <- ggplot(summary_predgrid_all, aes(y=mlat, x=mlon)) +
   geom_polygon(data = w, aes(x = long, y = lat, group = group), fill = "grey80") +
@@ -62,13 +65,16 @@ p_sd_g0 <- ggplot(summary_predgrid_all, aes(y=mlat, x=mlon)) +
   geom_contour_filled(aes(z=sdd_g0), breaks=colour_cats) +
   geom_point(aes(y=mlat, x=mlon), data=subset(fin, count>0),
              shape = 21, colour = "black", fill = "white", size=0.75) +
-#  scale_colour_viridis_d(position="bottom") +
+  scale_colour_viridis_d(drop=FALSE) +
 #  scale_fill_viridis_d(limits=range(colour_cats), 
 #                       position="bottom") +
-  #theme(legend.position = "bottom") +
   labs(x="", y="", fill="Standard\nerror") +
   coord_map(ylim=range(summary_predgrid_all$mlat), xlim=range(summary_predgrid_all$mlon)) +
-  theme_minimal()
+  theme_minimal() +
+  theme(legend.position = "bottom",
+        legend.text = element_text(size=12),
+        legend.title = element_text(size=12),
+        axis.text = element_text(size=12))
 print(p_sd_g0)
 
 ggsave(p_sd_g0, file="figures/unc_d_g0.pdf", width=7, height=9)
@@ -89,18 +95,20 @@ ggsave(p_sd_g0, file="figures/unc_d_g0.pdf", width=7, height=9)
 
 
 # plot yearly predictions and uncertainties
-summary_predgrid_yearly$avv_d <- cut(summary_predgrid_yearly$avv, c(0,0.001,.0023,.0036,.0085,.036,1))
+#summary_predgrid_yearly$avv_d <- cut(summary_predgrid_yearly$avv, c(0,0.001,.0023,.0036,.0085,.036,1))
 # remove NAs
-summary_predgrid_yearly <- summary_predgrid_yearly[!is.na(summary_predgrid_yearly$avv_d), ]
+#summary_predgrid_yearly <- summary_predgrid_yearly[!is.na(summary_predgrid_yearly$avv_d), ]
 # standard deviations
-summary_predgrid_yearly$sdd_d <- cut(summary_predgrid_yearly$sdd, c(0,0.001,.0023,.0036,.0085,.036,1, 2))
+#summary_predgrid_yearly$sdd_d <- cut(summary_predgrid_yearly$sdd, c(0,0.001,.0023,.0036,.0085,.036,1, 2))
 
 # make the data long rather than wide
 summary_predgrid_yearly <- summary_predgrid_yearly %>%
   # only need these columns
-  select(mlon, mlat, year, avv_d, sdd_d) %>%
+  #select(mlon, mlat, year, avv_d, sdd_d) %>%
+  select(mlon, mlat, year, avv, sdd) %>%
   # make a long rather than wide data.frame
-  pivot_longer(c(avv_d, sdd_d))
+  #pivot_longer(c(avv_d, sdd_d))
+  pivot_longer(c(avv, sdd))
 
 # name the data types correctly
 summary_predgrid_yearly$name <- factor(summary_predgrid_yearly$name,
@@ -109,7 +117,8 @@ summary_predgrid_yearly$name <- factor(summary_predgrid_yearly$name,
 # make the plot
 p_yearly <- ggplot(summary_predgrid_yearly, aes(y=mlat, x=mlon)) +
   geom_polygon(data = w, aes(x = long, y = lat, group = group), fill = "grey80") +
-  geom_tile(aes(fill=value)) +
+  #geom_tile(aes(fill=value)) +
+  geom_contour_filled(aes(z=sdd), breaks=colour_cats) +
   geom_point(aes(y=mlat, x=mlon), data=subset(fin, count>0),
              shape = 21, colour = "black", fill = "white", size=0.75) +
   scale_fill_viridis_d() +
@@ -117,7 +126,10 @@ p_yearly <- ggplot(summary_predgrid_yearly, aes(y=mlat, x=mlon)) +
   labs(x="", y="", fill="Value") +
   coord_map(ylim=range(summary_predgrid_yearly$mlat), xlim=range(summary_predgrid_yearly$mlon)) +
   theme_minimal() +
-  theme(legend.position = "bottom")
+  theme(legend.position = "bottom",
+        legend.text = element_text(size=12),
+        legend.title = element_text(size=12),
+        axis.text = element_text(size=12))
 #print(p_yearly)
 
 ggsave(p_yearly, file="figures/yearlies.pdf", width=10, height=9)
